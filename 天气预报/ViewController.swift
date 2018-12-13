7683552c220db31d8c895de9d294f1e9943a3857
刘季和 <863824147@qq.com>
2018-12-13 10:00:41 +0800

    新程序
    大体结构形成

---
+++ 天气预报/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  天气预报
+//
+//  Created by LJH on 2018/12/13.
+//  Copyright © 2018 LJH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func btnPressed(_ sender: AnyObject) {
+        print("Button Clicked")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
